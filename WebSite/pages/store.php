6ddf4d67f363d467cc14c516d02829ef63a46b28
KaueAnjos <kaueramada06@gmail.com>
2024-06-18 21:37:37 -0300

    Adicionando funcionanlidades ao display de compras

--- WebSite/pages/store.php
+++ WebSite/pages/store.php
@@ -1,46 +1,82 @@
 <head>
-<link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>/assets/css/store-style.css">
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>/assets/css/store-style.css">
 </head>
 
 <main>
     <section class="product-header">
         <div class="product-display">
 
+            <div class="product-exposition">
+                <img class="main-img" src="<?php echo INCLUDE_PATH; ?>/assets/img/products/tn1.png">
+                <div class="thumbnail-gallery">
+                <div><img src="<?php echo INCLUDE_PATH; ?>/assets/img/products/tn1.png"></div>
+                <div><img src="<?php echo INCLUDE_PATH; ?>/assets/img/products/tn2.png"> </div>
+                <div><img src="<?php echo INCLUDE_PATH; ?>/assets/img/products/tn3.png"></div>
+                <div><img src="<?php echo INCLUDE_PATH; ?>/assets/img/products/tn4.png"></div>
+                </div>
+            </div>
         </div>
+
         <aside class="product-details">
-            <h1>Nike</h1>
-            <h1>Tênis nike air max plus masculino</h1>
-            <p>cod do produto</p>
-            <br>
-            <h2>R$ 1.299,99</h2>
-            <h2>10x de 129,99</h2>
-            <br>
-            <p>Outras opções de cores:</p>
-            <div class="other-colors">
-                <div class="product-color"></div>
-                <div class="product-color"></div>
-                <div class="product-color"></div>
-                <div class="product-color"></div>
+
+            <!-- Product header -->
+            <div class="product-header">
+                <h1 class="brand-name">Nike</h1>
+                <h1 class="product-title">Tênis nike air max plus masculino</h1>
+                <p class="product-code">Código do produto</p>
             </div>
-            <br>
-            <p>Tamanho:</p>
-            <div class="product-size">
-                <div class="size"></div>
-                <div class="size"></div>
-                <div class="size"></div>
-                <div class="size"></div>
-                <div class="size"></div>
-                <div class="size"></div>
-                <div class="size"></div>
-                <div class="size"></div>
-                <div class="size"></div>
-                <div class="size"></div>
+
+            <!-- Product price -->
+            <div class="product-pricing">
+                <h2 class="product-price">R$ 1.299,99</h2>
+                <h2 class="product-installments">10x de 129,99</h2>
             </div>
+
+
+            <!-- Color Optios -->
+            <div class="color-options">
+                <p class="color-options-title">Outras opções de cores:</p>
+                <div class="other-colors">
+                    <div class="product-color"></div>
+                    <div class="product-color"></div>
+                    <div class="product-color"></div>
+                    <div class="product-color"></div>
+                </div>
+            </div>
+
+            <div class="size-options">
+                <p class="size-options-title">Tamanho:</p>
+                <div class="product-size">
+                    <input type="radio" id="size-36" name="size" class="size-input" />
+                    <label for="size-36" class="size">36</label>
+                    <input type="radio" id="size-37" name="size" class="size-input" />
+                    <label for="size-37" class="size">37</label>
+                    <input type="radio" id="size-38" name="size" class="size-input" />
+                    <label for="size-38" class="size">38</label>
+                    <input type="radio" id="size-39" name="size" class="size-input" />
+                    <label for="size-39" class="size">39</label>
+                    <input type="radio" id="size-40" name="size" class="size-input" />
+                    <label for="size-40" class="size">40</label>
+                    <input type="radio" id="size-41" name="size" class="size-input" />
+                    <label for="size-41" class="size">41</label>
+                    <input type="radio" id="size-42" name="size" class="size-input" />
+                    <label for="size-42" class="size">42</label>
+                    <input type="radio" id="size-43" name="size" class="size-input" />
+                    <label for="size-43" class="size">43</label>
+                    <input type="radio" id="size-44" name="size" class="size-input" />
+                    <label for="size-44" class="size">44</label>
+                    <input type="radio" id="size-45" name="size" class="size-input" />
+                    <label for="size-45" class="size">45</label>
+                </div>
+            </div>
+
 
             <div class="buy-buttons">
                 <button>Comprar agora</button>
                 <button>Adicionar ao Carrinho</button>
             </div>
         </aside>
+
+
     </section>
 </main>
